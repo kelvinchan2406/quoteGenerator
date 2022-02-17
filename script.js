9122b5eaff37cde09a479117be2a5672b9e1d6ce
kelvinchan2406 <kchk55555@gmail.com>
2022-02-17 14:32:55 +0800

    added counter for error

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const getQuoteId = document.getElementById("quote");
 const getAuthorId = document.getElementById("author");
-const getButton = document.getElementById("refresh");
+const getTweetButton = document.getElementById("tweet");
+const getRefreshButton = document.getElementById("refresh");
 
 
 let data = [];
 let quote = ``;
 let author = ``;
+let counter = 0;
+
+// const addCounter = () => {
+//     counter++;
+//     // (!(counter === 10)) ? counter = counter + 1 : getQuoteId.textContent = `cant get quotes`;
+//     console.log(counter);
+// }
+
 
 const assignQuote = () => {
     getQuoteId.textContent = quote;
     getAuthorId.textContent= author;
+    // counter = 0;
 }
 
 const getRndQuote = () => {
@@ -28,12 +38,33 @@ async function getQuote() {
         getQuoteId.textContent = `Loading`;
         const response = await fetch(api);
         data = await response.json();
+        // throw new Error('oops');
         getRndQuote();
     }catch {
-        getQuote();
         console.log(`cants get api`);
+        console.log(counter);
+        counter++;
+        runCounter();
+        // (counter < 10) ? (counter = counter++) : (getQuoteId.textContent = `cant get quotes`);
+        // (counter < 10) ? (getQuoteId.textContent = `cant get quotes`) : (counter = counter++);
+        // if(counter > 10){
+        //     counter = counter++;
+        // }else{
+        //     getQuoteId.textContent = `cant get quotes`
+        // }
     }
 }
+const runCounter = () => {
+    (!(counter === 10)) ? getQuote() : (getQuoteId.textContent = `cant get quotes`);
+}
 
-getButton.addEventListener('click', getQuote);
-getQuote();
+
+const tweet = () => {
+
+    window.open(`https://twitter.com/intent/tweet?text=${quote} - ${author}`, "_blank");
+}
+
+
+getTweetButton.addEventListener('click', tweet);
+getRefreshButton.addEventListener('click', getQuote);
+runCounter();
